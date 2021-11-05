a3045ddfa43ff9fb9e48c507a110dde5b067a7aa
Seth Allen <sethallen44.sa@gmail.com>
2021-11-05 18:39:02 -0400

    copy test

---
+++ src/solution.js
@@ -0,0 +1,32 @@
+/*
+  Write each function according to the instructions.
+  
+  When a function's parameters reference `cart`, it references an object that looks like the one that follows.
+  {
+    "Gold Round Sunglasses": { quantity: 1, priceInCents: 1000 },
+    "Pink Bucket Hat": { quantity: 2, priceInCents: 1260 }
+  }
+*/
+
+function calculateCartTotal(cart) {
+let result = 0 ;
+for (let items in cart){
+  result += cart[items].priceInCents * cart[items].quantity;
+}
+  return result
+}
+
+function printCartInventory(cart) {
+  let itemQuantity = "";
+  for (let itemKeys in cart) { 
+    const array = cart[itemKeys]; 
+    const measure = array.quantity; 
+    itemQuantity += `${measure}x${itemKeys}\n`;
+}
+return itemQuantity;
+  }
+
+module.exports = {
+  calculateCartTotal,
+  printCartInventory,
+};
